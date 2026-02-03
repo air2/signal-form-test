186a27dbc3f7e5d3db2b6de257be3be5298be9ee
air2 <github@air2.dds.nl>
2026-02-03 11:04:31 +0100

    added output

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { Component, model, OnInit, signal } from '@angular/core';
+import { Component, computed, linkedSignal, model, OnInit, signal } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
   applyWhen,
+  applyWhenValue,
   form,
   FormField,
   required,
@@ -11,26 +12,67 @@ import {
 
 @Component({
   selector: 'app-root',
-  template: `Hello: {{this.type()}}`,
+  template: `Hello: {{this.whenValidated()}}<br/>
+  Hello2: {{this.whenValidated2()}}<br />
+  Hello3: {{this.whenValidated3()}}
+  <button (click)="onSubmit()" type="button">click</button>`,
 })
 export class Playground implements OnInit {
+  readonly type = signal<string | undefined>(undefined);
+  readonly typeOrDefault = computed(()=>this.type() ?? 'Test')
   readonly data = model({
-    type: 'Test',
+    Type: this.typeOrDefault(),
     firstName: 'test',
   });
-  readonly type = signal<string | undefined>(undefined);
+
+  readonly whenValidated = signal<string>('x')
+  readonly whenValidated2 = signal<string>('')
+  readonly whenValidated3 = signal<string>('')
   readonly formData = form(this.data, (schemaPath) => {
     applyWhen(
       schemaPath,
-      () => this.type() !== undefined,
+      (ctx) => ctx.valueOf(schemaPath.Type) === 'Updated',
       (ctx) => {
         console.log('applyWhen', this.type());
+        this.whenValidated.set(this.type() ?? '')
         required(ctx.firstName);
+      }
+    );
+    applyWhenValue(
+      schemaPath,
+      (ctx) => ctx.Type === 'Updated',
+      (ctx) => {
+        console.log('applyWhenValue', this.type());
+        this.whenValidated2.set(this.type() ?? '')
+        required(ctx.firstName);
+      }
+    );
+
+    applyWhen(
+      schemaPath.Type,
+      (ctx) => ctx.value() === 'Updated',
+      (ctx) => {
+        console.log('applyWhen 3', this.type());
+        this.whenValidated3.set(this.type() ?? '')
+        required(ctx);
+      }
+    );
+    applyWhenValue(
+      schemaPath.Type,
+      (ctx) => ctx === 'Updated',
+      (ctx) => {
+        console.log('applyWhen 3', this.type());
+        this.whenValidated3.set(this.type() ?? '')
+        required(ctx);
       }
     );
   });
 
   ngOnInit() {
+  }
+  
+  onSubmit() {
+    
     this.type.set('Updated');
   }
 }
